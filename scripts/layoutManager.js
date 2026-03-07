1836eefcfcf728fa5d1547a3c8a8484103ec95e3
Kareem <kareem.sinbel@gmail.com>
2026-03-08 00:59:21 +0200

    Added Templates for home | Dynamic script | Removed files

---
+++ scripts/layoutManager.js
@@ -0,0 +1,73 @@
+const LayoutManager = (function () {
+
+    const templates = {};
+    const basePath = "../components/";
+
+    async function loadTemplate(name) {
+
+        if (templates[name]) 
+            return templates[name];
+
+        try
+        {
+            const res = await fetch(`${basePath}${name}_Template.html`);
+
+            if(!res.ok)
+            throw new Error(`Component ${name} not found`);
+
+            const html = await res.text();
+
+            templates[name] = html;
+
+            return html;
+        }
+        catch(err)
+        {
+            console.error(err);
+            return `<div style="color:red">Component "${name}" failed to load</div>`;
+        }
+    }
+
+    async function renderComponents() {
+
+        const components = document.querySelectorAll("[data-component]");
+
+        // for (const element of components) {
+
+        //     const name = element.dataset.component;
+
+        //     const template = await loadTemplate(name);
+
+        //     element.innerHTML = template;
+
+        // }
+
+        const tasks = [...components].map(async element =>
+        {
+            const name = element.dataset.component;
+
+            const template = await loadTemplate(name);
+
+            element.innerHTML = template;
+        });
+
+        await Promise.all(tasks);
+    }
+
+    async function init() {
+
+        await renderComponents();
+
+    }
+
+    return {
+        init
+    };
+
+})();
+
+
+document.addEventListener("DOMContentLoaded", async () => 
+{
+    await LayoutManager.init();
+});

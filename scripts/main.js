416a134bd212a204ef2acbd7c04c099fad533718
Kareem <kareem.sinbel@gmail.com>
2026-03-15 01:50:26 +0200

    Product Details SPA added

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,9 @@ import { Router } from "./router.js";
 
 document.addEventListener("LayoutBuilt", () => 
 {
-    Router.addRoute("home", "../pages/home.html", "../html/pages/home.js", "../../stylesheets/Home/Home.css");
+    Router.addRoute("home", "../Home/home.html", "./Home/home.js", "../../stylesheets/Home/Home.css");
+    Router.addRoute("productDetails", "../Product Details/productDetails.html","./Product Details/productDetails.js" ,"../../stylesheets/Product Details/productDetails.css");
+
     const page = new URLSearchParams(window.location.search).get('page') || "home";
     Router.navigate(page);
 });

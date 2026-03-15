c285c8ec34474624cbacb56af3b2015b2df890ba
Kareem <kareem.sinbel@gmail.com>
2026-03-15 12:30:56 +0200

    Navbar fixed  | layoutManager navigate home fixed

--- scripts/layoutManager.js
+++ scripts/layoutManager.js
@@ -113,36 +113,34 @@ document.addEventListener("DOMContentLoaded", async () =>
     //     nav.classList.add("d-lg-flex");
     //     nav.classList.remove("d-none");    
     // }
-const user = getLoggedInUser();
+    const user = getLoggedInUser();
 
-if(!checkAuth())
-{
-    
-    document.getElementById("user-name").style.display = "none";
-    document.getElementById("logout-btn").style.display = "none";
-    document.getElementById("icons-container").classList.add("d-none") ;
-    document.getElementById("icons-container").classList.remove("d-lg-flex") ;
+    if(!checkAuth())
+    {
+        
+        document.getElementById("user-name").style.display = "none";
+        document.getElementById("logout-btn").style.display = "none";
+        document.getElementById("icons-container").classList.add("d-none") ;
+        document.getElementById("icons-container").classList.remove("d-lg-flex") ;
 
-    document.getElementById("login-btn").style.display = "block";
+        document.getElementById("login-btn").style.display = "block";
 
-    document.getElementById("login-btn").addEventListener("click", function(){
-        window.location.href = "../../html/Auth/login.html";
-    });
-}
-else
-{
-    
-    document.getElementById("login-btn").style.display = "none";
-     document.getElementById("icons-container").classList.remove("d-none") ;
-    document.getElementById("icons-container").classList.add("d-lg-flex") ;
-    if(user){
-        document.getElementById("user-name").innerText = user.username || user.name;
+        document.getElementById("login-btn").addEventListener("click", function(){
+            window.location.href = "../../html/Auth/login.html";
+        });
     }
+    else
+    {
+        document.getElementById("login-btn").style.display = "none";
+        document.getElementById("icons-container").classList.remove("d-none") ;
+        document.getElementById("icons-container").classList.add("d-lg-flex") ;
+        if(user){
+            document.getElementById("user-name").innerText = user.username || user.name;
+        }
 
-    document.getElementById("logout-btn").addEventListener("click", function(){
-        deleteCookie("loggedInUser");
-        window.location.href = "../../html/Home/home.html";
-    });
-}
-
+        document.getElementById("logout-btn").addEventListener("click", function(){
+            deleteCookie("loggedInUser");
+            window.location.href = "../../html/Main Page/index.html";
+        });
+    }
 });

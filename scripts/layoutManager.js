0464977dd264478db3a5124b656b8de1f3456f60
Kareem <74905162+KareemSinbel@users.noreply.github.com>
2026-03-15 11:59:40 +0200

    Dev to spa Nav fixes (#20)
    * features/displayRelatedProducts (#16)
    
    * features/displayRelatedProducts
    
    * Add exception handling in cart.js
    
    ---------
    
    Co-authored-by: Kareem <74905162+KareemSinbel@users.noreply.github.com>
    
    * errors/fixcartlogicalerror (#17)
    
    * edit user icon and add username, login, logout in layout manager (#19)
    
    Co-authored-by: abdullahtarek <student@iti.gov.eg>
    
    ---------
    
    Co-authored-by: Omar Nabil <omarabosalem66@gmail.com>
    Co-authored-by: Abdullah Tarek <137053608+atg8900@users.noreply.github.com>
    Co-authored-by: abdullahtarek <student@iti.gov.eg>

--- scripts/layoutManager.js
+++ scripts/layoutManager.js
@@ -101,38 +101,48 @@ document.addEventListener("DOMContentLoaded", async () =>
        Router.navigate("cart"); 
     });
 
-    if(!checkAuth())
-    {
-        let nav = document.getElementById("icons-container");
-        nav.classList.remove("d-lg-flex");
-        nav.classList.add("d-none");    
+    // if(!checkAuth())
+    // {
+    //     let nav = document.getElementById("icons-container");
+    //     nav.classList.remove("d-lg-flex");
+    //     nav.classList.add("d-none");    
+    // }
+    // else
+    // {
+    //     let nav = document.getElementById("icons-container");
+    //     nav.classList.add("d-lg-flex");
+    //     nav.classList.remove("d-none");    
+    // }
+const user = getLoggedInUser();
+
+if(!checkAuth())
+{
+    
+    document.getElementById("user-name").style.display = "none";
+    document.getElementById("logout-btn").style.display = "none";
+    document.getElementById("icons-container").classList.add("d-none") ;
+    document.getElementById("icons-container").classList.remove("d-lg-flex") ;
+
+    document.getElementById("login-btn").style.display = "block";
+
+    document.getElementById("login-btn").addEventListener("click", function(){
+        window.location.href = "../../html/Auth/login.html";
+    });
+}
+else
+{
+    
+    document.getElementById("login-btn").style.display = "none";
+     document.getElementById("icons-container").classList.remove("d-none") ;
+    document.getElementById("icons-container").classList.add("d-lg-flex") ;
+    if(user){
+        document.getElementById("user-name").innerText = user.username || user.name;
     }
-    else
-    {
-        const loggedInUserStr = getLoggedInUser();
-        let isSeller = false;
 
-        if (loggedInUserStr) 
-        {
-            try {
-                if (loggedInUserStr.role === 'seller') {
-                    isSeller = true;
-                }
-            } catch (e) {
-                console.error("Error checking roles", e);
-            }
-        }
+    document.getElementById("logout-btn").addEventListener("click", function(){
+        deleteCookie("loggedInUser");
+        window.location.href = "../../html/Home/home.html";
+    });
+}
 
-        if (!isSeller) 
-        {
-            const productNavItem = document.getElementById('product');
-            if (productNavItem) {
-                productNavItem.remove();
-            }
-        }
-
-        let nav = document.getElementById("icons-container");
-        nav.classList.add("d-lg-flex");
-        nav.classList.remove("d-none");    
-    }
 });

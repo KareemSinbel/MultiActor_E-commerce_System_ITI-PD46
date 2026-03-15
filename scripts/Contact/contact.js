bc91ec8194af509910a5501d7f2bc1ab480732bb
Mostafa <hussinym2@gmail.com>
2026-03-16 00:01:17 +0200

    fix bug on contact

--- scripts/Contact/contact.js
+++ scripts/Contact/contact.js
@@ -1,24 +1,26 @@
 import { showBootstrapToast,toggleBreadcrumb } from "../helpers.js";
 import { Router } from "../router.js";
-const messgebtn = document.getElementById("messagebtn")
 
-const form = document.getElementById("contactForm");
 
 
 document.addEventListener("pageLoaded", (e) => 
 {
+    const messgebtn = document.getElementById("messagebtn")
+
+    const form = document.getElementById("contactForm");
     if (e.detail.page === "contact") 
     {
         toggleBreadcrumb("Contact", true);
 
         messgebtn.addEventListener("click", function(){
-
+            console.log("clicked")
             if (!form.checkValidity()) {
-                e.preventDefault(); 
+                //e.preventDefault(); 
                 alert("Please fill all fields.");
             }else {
-                e.preventDefault()
-                e.stopImmediatePropagation()
+                //e.preventDefault()
+                console.log("hello")
+                // e.stopImmediatePropagation()
                 showBootstrapToast("thasnks for your message") 
                 Router.navigate("home")
             }
